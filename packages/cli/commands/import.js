93c571172e494f79074fb8ce533bdab0d0f4a6ca
Chris Anderson <jchris@gmail.com>
2023-05-18 10:23:32 -0700

    trying stuff

--- packages/cli/commands/import.js
+++ packages/cli/commands/import.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { Text } from 'ink'
+import Spinner from 'ink-spinner'
 
 /// Import data into a database
-const Import = ({ database, file }) => <Text>Importing {file} to {database}</Text>
+const Import = ({ database, file }) => {
+  return <Text><Spinner />Importing {file} to {database}</Text>
+}
 
 Import.propTypes = {
   /// Name of the database to use, will create if necessary
