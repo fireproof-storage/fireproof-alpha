06f91eab8f5593ca908dd0c9c1043a78be5a9b9c
Chris Anderson <jchris@gmail.com>
2023-05-18 10:39:21 -0700

    fuck spinner

--- packages/cli/commands/import.js
+++ packages/cli/commands/import.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { Text } from 'ink'
-import Spinner from 'ink-spinner'
 
 /// Import data into a database
 const Import = ({ database, file }) => {
-  return <Text><Spinner />Importing {file} to {database}</Text>
+  return <Text>Importing {file} to {database}</Text>
 }
 
 Import.propTypes = {
