e3f0db19207234e7db2c6a3a4e06eff13d4487ac
Chris Anderson <jchris@gmail.com>
2023-05-18 10:13:29 -0700

    import command

---
+++ packages/cli/commands/import.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Text } from 'ink'
+
+/// Import data into a database
+const Import = ({ database, file }) => <Text>Importing {file} to {database}</Text>
+
+Import.propTypes = {
+  /// Name of the database to use, will create if necessary
+  database: PropTypes.string.isRequired,
+  /// Path to a JSON file to import
+  file: PropTypes.string.isRequired
+}
+Import.positionalArgs = ['database', 'file']
+
+export default Import
