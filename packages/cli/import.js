fe7758b3b703ef5c01ddf98b839584292215a265
Chris Anderson <jchris@gmail.com>
2023-05-18 10:36:42 -0700

    newer ink

---
+++ packages/cli/import.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { Text } from 'ink'
+import Spinner from 'ink-spinner'
+
+/// Import data into a database
+const Import = ({ database, file }) => {
+  return <Text><Spinner />Importing {file} to {database}</Text>
+}
+
+Import.propTypes = {
+  /// Name of the database to use, will create if necessary
+  database: PropTypes.string.isRequired,
+  /// Path to a JSON file to import
+  file: PropTypes.string.isRequired
+}
+Import.positionalArgs = ['database', 'file']
+
+export default Import
